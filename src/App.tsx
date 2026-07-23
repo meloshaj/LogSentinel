2e5ca50fef7dbc83126ef4658564a9dc6e4909a4
BlertSylejmani1 <blert.sylejmani@student.uni-pr.edu>
2026-07-23 18:12:37 +0200

    Add React Flow engine setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { ReactFlowProvider } from "@xyflow/react";
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <ReactFlowProvider>
+      <RouterProvider router={router} />
+    </ReactFlowProvider>
+  );
 }
